2d793eca16617f6daaaff705aaaeb12969593c48
Sahil Mund <msahilranjan@gmail.com>
2025-10-26 11:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+	const prices = document.querySelectorAll('.price');
+
+	let total = 0;
+	prices.forEach((p) => {
+		total += parseFloat(p.textContent)
+	});
+
+	const table = document.querySelector('table');
+	
+	const newRow = document.createElement("tr");
+
+	const labelCell = document.createElement("td");
+	const totalCell = document.createElement("td");
+
+	totalCell.textContent = total;
+	labelCell.textContent = 'Total Price: ';
+
+	newRow.append(labelCell,totalCell);
+	table.appendChild(newRow)
   
 };
 
